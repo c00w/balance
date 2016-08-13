8135fe51aa3ba7f5edf973046151f70a8b813bd4
Colin L. Rice <colin@daedrum.net>
2016-08-13 16:17:03 -0400

    Make program better

--- main.go
+++ main.go
@@ -4,42 +4,61 @@ import (
 	"fmt"
 )
 
-func main() {
-	us_stock := 0.0
-	us_bond := 0.0
-	int_stock := 0.0
-	int_bond := 0.0
-	gain := 0.0
+type class struct {
+	human_string  string
+	desired_ratio float64
+}
 
-	fmt.Print("What is your current US Stock Amount? :")
-	fmt.Scan(&us_stock)
-	fmt.Print("International Stock? :")
-	fmt.Scan(&int_stock)
-	fmt.Print("US Bond? :")
-	fmt.Scan(&us_bond)
-	fmt.Print("International Bond? :")
-	fmt.Scan(&int_bond)
-	fmt.Print("How much do you want to purchase? :")
-	fmt.Scan(&gain)
+func main() {
 
 	dom_to_int := 0.7
 	stock_to_bond := 0.9
+	classes := []class{
+		{"US Stock", dom_to_int * stock_to_bond},
+		{"International Stock", (1 - dom_to_int) * stock_to_bond},
+		{"US Bond", dom_to_int * (1 - stock_to_bond)},
+		{"International Bond", (1 - dom_to_int) * (1 - stock_to_bond)},
+	}
 
-	total := gain + us_stock + us_bond + int_stock + int_bond
+	balances := make(map[int]float64)
+	for i, c := range classes {
+		fmt.Printf("What is your current %s Amount? : ", c.human_string)
+		balance := 0.0
+		fmt.Scan(&balance)
+		balances[i] = balance
+	}
 
-	us_stock_desired := dom_to_int * stock_to_bond * total
-	fmt.Printf("You should have %f in US Stock, have %f delta %f\n",
-		us_stock_desired, us_stock, us_stock_desired-us_stock)
+	gain := 0.0
+	fmt.Print("How much do you want to purchase? : ")
+	fmt.Scan(&gain)
 
-	int_stock_desired := (1 - dom_to_int) * stock_to_bond * total
-	fmt.Printf("You should have %f in International Stock, have %f delta %f\n",
-		int_stock_desired, int_stock, int_stock_desired-int_stock)
+	total := gain
+	for _, b := range balances {
+		total += b
+	}
 
-	us_bond_desired := dom_to_int * (1 - stock_to_bond) * total
-	fmt.Printf("You should have %f in US Bonds, have %f delta %f\n",
-		us_bond_desired, us_bond, us_bond_desired-us_bond)
+	deltas := make(map[int]float64)
+	for i, c := range classes {
+		desired := c.desired_ratio * total
+		deltas[i] = desired - balances[i]
+		fmt.Printf("You should have %f in %s, have %f delta %f\n",
+			desired, c.human_string, balances[i], deltas[i])
+	}
 
-	int_bond_desired := (1 - dom_to_int) * (1 - stock_to_bond) * total
-	fmt.Printf("You should have %f in International Bonds, have %f delta %f\n",
-		int_bond_desired, int_bond, int_bond_desired-int_bond)
+	purchase_orders := make(map[int]float64)
+	purchase_sum := 0.0
+	for i, c := range deltas {
+		if c > 0.0 {
+			purchase_orders[i] = c
+			purchase_sum += c
+		}
+	}
+
+	scale := gain / purchase_sum
+	for i, c := range purchase_orders {
+		purchase_orders[i] = c * scale
+		fmt.Printf("You should buy %f %ss\n",
+			purchase_orders[i], classes[i].human_string)
+	}
+
 }
